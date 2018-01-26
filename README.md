ec9eafc651161f69fa39f346fa9281841be0f872
devopsralf <ralf@dreamitsystems.com>
2018-01-26 14:38:25 -0500

    Fixes: Remove Karbo Traces, Update Pools, Update README

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Block explorer for TurtleCoin CryptoNote based cryptocurrency.
 
 1) It takes data from daemon turtlecoind. It should be accessible from the Internet. Run turtlecoind with open port as follows:
 ```bash
-./turtlecoind --restricted-rpc --enable-cors=* --enable-block_explorer --rpc-bind-ip=0.0.0.0 --rpc-bind-port=11898
+./TurtleCoind --restricted-rpc --enable-cors=* --enable_blockexplorer --rpc-bind-ip=0.0.0.0 --rpc-bind-port=11898
 ```
 2) Just upload to your website and change 'api' variable in config.js to point to your daemon.
 
--- config.js
+++ config.js
@@ -7,8 +7,17 @@ var refreshDelay = 30000;
 var networkStat = {
     "trtl": [
 		["z-pool.com", "http://z-pool.com:8117"],
-		
-		
+		["eu.turtlepool.space", "http://eu.turtlepool.space:8117"],
+    ["us.turtlepool.space", "http://us.turtlepool.space:8117"],
+    ["hk.turtlepool.space", "http://hk.turtlepool.space:8117"],
+    ["turtlecoinpool.ddns.net", "http://turtlecoinpool.ddns.net:8127"],
+    ["slowandsteady.fun", "http://slowandsteady.fun:8119"],
+    ["trtl.mine2gether.com", "https://trtl.mine2gether.com/api"],
+    ["auspool.turtleco.in", "https://auspool.turtleco.in/api"],
+    ["trtl.radicaldelta.org", "http://trtl.radicaldelta.org:8117"],
+    ["turtle.atpool.party", "http://turtle-eu.atpool.party:8117"],
+    ["ny.minetrtl.us", "http://ny.minetrtl.us:8117"],
+		["xk.is", "https://xk.is/api"]
     ]
 };
 
