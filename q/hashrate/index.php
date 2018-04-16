a7af037545c4963e453341e27b310c530318f580
pypper <pypper@protonmail.com>
2018-04-16 11:16:27 -0400

    make API great again!

--- q/hashrate/index.php
+++ q/hashrate/index.php
@@ -1,13 +1,10 @@
-<?php 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_URL, 'http://127.0.0.1:11898/getinfo');
-$result = curl_exec($ch);
-$obj = json_decode($result, TRUE);
-curl_close($ch);
-//print_r($obj);
-$difficulty = $obj['difficulty'];
-$hashrate = round($difficulty / 30);
+<?php
+require '../util.php';
+$config = (require '../../config.php');
+
+$info = fetch_getinfo($config['api']);
+
+$difficulty = $info['difficulty'];
+$hashrate = round($difficulty / $config['blockTargetInterval']);
+
 print_r($hashrate);
-?>
