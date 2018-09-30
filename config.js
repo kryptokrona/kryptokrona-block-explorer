0accdd0c4458ca6c39ebf06b68cc1dd7eed9a249
Seimax <maximilian90seidel@gmail.com>
2018-09-30 21:02:06 +0200

    Update config.js
    Added trtl.hackerknowledge.de PPS Pool

--- config.js
+++ config.js
@@ -17,7 +17,8 @@ var networkStat = {
 	["trtl.radicaldelta.org", "http://trtl.radicaldelta.org:8117"],
 	["turtle.atpool.party", "http://turtle-eu.atpool.party:8117"],
 	["ny.minetrtl.us", "http://ny.minetrtl.us:8117"],
-	["xk.is", "https://xk.is/api"]
+	["xk.is", "https://xk.is/api"],
+	["trtl.hackerknowledge.de", "https://pool.trtl.hackerknowledge.de:8119"]
  ]
 };
 
