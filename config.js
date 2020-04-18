df9199949cf313f755f63f24a2397beb1c086651
ux33-331 <61588635+ux33-331@users.noreply.github.com>
2020-04-18 12:12:02 +0200

    fix hashrate

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var api = 'http://explorer.kryptokrona.se:11898';
 var donationAddress = "SEKReX2avthCKT4YUUKV3jgZ1Hderk9XbRciqp8vHVPoDSb9nA1dCV86Jia3TkD4jWgfxeh1AEYV3DKEAesSb7mSAvNqf6cB6kR";
-var blockTargetInterval = 30; // enter the block interval in seconds
+var blockTargetInterval = 90; // enter the block interval in seconds
 var coinUnits = 100;  // enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 trtl
 var totalSupply =  100000000000000; // enter the total supply in atomic units
 var symbol = 'XKR'; // enter the coin's ticker
--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 return array(
         'api' => 'http://127.0.0.1:11898',
-        'blockTargetInterval' => 30,
+        'blockTargetInterval' => 90,
         'coinUnits' => 100
 );
