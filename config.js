fe6b5da40c4aeaf1dcb42250024b654679b62363
ux33-331 <61588635+ux33-331@users.noreply.github.com>
2020-05-13 21:58:57 +0200

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ var networkStat = {
  "xkr": [
 	["pool.kryptokrona.se", "https://explorer.kryptokrona.se/pool1/"],
 	["pool2.kryptokrona.se", "https://explorer.kryptokrona.se/pool2/"],
-	["pool4.kryptokrona.se", "https://explorer.kryptokrona.se/pool4/"]
+	["pool4.kryptokrona.se", "http://pool4.kryptokrona.se:8117"]
 	//["pool-name", "pool-url:port"],
 	//["pool-name", "pool-url:port"]
  ]
