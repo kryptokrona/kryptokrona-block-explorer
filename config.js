8b3f46325dafe41cb06771a031f5ebf435f5a36f
ux33-331 <61588635+ux33-331@users.noreply.github.com>
2020-11-17 14:33:56 +0100

    fix gabber/spooky/poolpay/letshash

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var api = 'https://explorer.kryptokrona.se/api';
 var donationAddress = "SEKReX2avthCKT4YUUKV3jgZ1Hderk9XbRciqp8vHVPoDSb9nA1dCV86Jia3TkD4jWgfxeh1AEYV3DKEAesSb7mSAvNqf6cB6kR";
 var blockTargetInterval = 90;
-var coinUnits = 100000; 
+var coinUnits = 100000;
 var totalSupply =  100000000000000;
 var symbol = 'XKR';
 var refreshDelay = 30000;
@@ -16,11 +16,13 @@ var networkStat = {
  "xkr": [
     ["pool.kryptokrona.se", "https://explorer.kryptokrona.se/pool1/"],
     ["pool2.kryptokrona.se", "https://explorer.kryptokrona.se/pool2/"],
-	["swepool.org", "https://swepool.org/api"],
-	["floki.kryptokrona.se", "https://floki.kryptokrona.se/api"],
+  	["swepool.org", "https://swepool.org/api"],
+  	["floki.kryptokrona.se", "https://floki.kryptokrona.se/api"],
     ["pool.gamersnest.org", "https://explorer.kryptokrona.se/pool.gamersnest.org/"],
-    ["letshash.it", "https://letshash.it:8279/"],
-    ["gabberpool.nl", "http://gabberpool.nl:1121"]
+    ["letshash.it", "https://letshash.online:15040"],
+    ["gabberpool.nl/XKR", "https://gabberpool.nl:1122"],
+    ["Spookyplanet.nl", "https://spookyplanet.nl:1172"],
+    ["xkr.pool-pay.com", "https://xkr.pool-pay.com:5383"]
  ]
 };
 
