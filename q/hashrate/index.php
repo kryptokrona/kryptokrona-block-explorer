debb193864967b72ce0459ae728db49b347d3dea
fruktstav <fruktstav@gmail.com>
2021-03-15 13:57:25 +0000

    Readded explorer API

---
+++ q/hashrate/index.php
@@ -0,0 +1,18 @@
+<?php
+	$config = (require '../config.php');
+	$ch = curl_init();
+
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_URL, $api_url."/getinfo");
+
+	$result = curl_exec($ch);
+
+	$obj = json_decode($result, TRUE);
+	curl_close($ch);	
+
+	$difficulty = $obj['difficulty'];
+	$hashrate = round($difficulty / $blockTargetInterval);
+
+	print_r($hashrate);
+?>
---
+++ q/supply/index.php
@@ -0,0 +1,57 @@
+<?php
+
+$config = (require '../config.php');
+
+$data_string = '{"jsonrpc":"2.0","id":"test","method":"getlastblockheader","params":" "}';
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$api_url."/json_rpc");
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Content-Length: ' . strlen($data_string))
+);
+
+$result = curl_exec($ch);
+
+// Decode the response
+$responseData = json_decode($result, TRUE, 4);
+
+// Print the date from the response
+// print_r($responseData);
+
+$hash = $responseData['result']['block_header']['hash'];
+
+curl_close($ch);
+
+// ---
+
+$data_string2 = '{"jsonrpc":"2.0","id":"test","method":"f_block_json","params":{"hash":"'.$hash.'"}}';
+
+
+$ch2 = curl_init();
+curl_setopt($ch2, CURLOPT_URL, $api_url."/json_rpc");
+curl_setopt($ch2, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch2, CURLOPT_POSTFIELDS, $data_string2);
+curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch2, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Content-Length: ' . strlen($data_string2))
+);
+
+$block = curl_exec($ch2);
+
+// Decode the response
+$blockData = json_decode($block, TRUE);
+
+#print_r($blockData);
+
+$supplyRaw = $blockData['result']['block']['alreadyGeneratedCoins'];
+
+$supply  = number_format($supplyRaw / $coinUnits, 2, ".", "");
+
+print_r($supply);
+
+curl_close($ch2);
