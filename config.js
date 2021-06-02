d49657bd6dc51e2ba2f8c3b7ce51c4e6f6f8edef
TechyGuy <65623992+TechyGuy17@users.noreply.github.com>
2021-06-02 17:53:20 +0200

    Updated gamersnest to https

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ let pools = [ // Name, Site URL, API URL
   ["Swepool", "https://swepool.org", "https://swepool.org/api/stats"],
   ["Kryptokrona Pool2", "https://pool2.kryptokrona.se", "https://blocksum.org/pool2/stats"],
   ["Floki Kryptokrona Pool", "https://floki.kryptokrona.se", "https://floki.kryptokrona.se/api/stats"],
-  ["GamersNest", "http://pool.gamersnest.org", "https://blocksum.org/gamersnest/stats"],
+  ["GamersNest", "https://pool.gamersnest.org", "https://pool.gamersnest.org/api/stats"],
   ["PoolPay", "https://xkr.pool-pay.com", "https://xkr.pool-pay.com:5383/stats"],
   ["Infinium space", "https://xkr.pools.infinium.space", "https://apikz5ssx.infinium.space/xkr/xkr/v1/live_stats"]
 
