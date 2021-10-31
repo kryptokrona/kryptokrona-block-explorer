f86a60259f2a906df31401acc262a482b168d051
Lukas Nyberg <Lukas.nyberg@gmail.com>
2021-10-31 13:24:15 +0100

    removed closed pools

--- config.js
+++ config.js
@@ -5,10 +5,8 @@ let ticker = "XKR"
 
 let pools = [ // Name, Site URL, API URL
   ["Swepool", "https://swepool.org", "https://swepool.org/api/stats"],
-  ["Kryptokrona Pool2", "https://pool2.kryptokrona.se", "https://blocksum.org/pool2/stats"],
   ["GamersNest", "https://pool.gamersnest.org", "https://pool.gamersnest.org/api/stats"],
   ["PoolPay", "https://xkr.pool-pay.com", "https://xkr.pool-pay.com:5383/stats"],
-  ["Infinium space", "https://xkr.pools.infinium.space", "https://apikz5ssx.infinium.space/xkr/xkr/v1/live_stats"],
   ["Norpool", "https://norpool.org", "https://norpool.org/api/stats"],
   ["Privacymine", "https://privacymine.net", "https://privacymine.net:8117/stats"]
   //Removed some pools that were causing perfomance issues by not responding..
@@ -20,6 +18,8 @@ let pools = [ // Name, Site URL, API URL
   // ["ComPool", "https://kryptokrona.compool.net", "https://api.compool.net/xkr/v1/stats"],
   // ["WorldMining", "http://xkr-worldmining.servebeer.com", "https//blocksum.org/worldmining/stats"],
   // ["Kryptokrona Göta", "https://pool.kryptokrona.se", "https://gota.kryptokrona.se/api/stats"],
-  //["Floki Kryptokrona Pool", "https://floki.kryptokrona.se", "https://floki.kryptokrona.se/api/stats"],
+  // ["Floki Kryptokrona Pool", "https://floki.kryptokrona.se", "https://floki.kryptokrona.se/api/stats"],
+  // ["Infinium space", "https://xkr.pools.infinium.space", "https://apikz5ssx.infinium.space/xkr/xkr/v1/live_stats"],
+  // ["Kryptokrona Pool2", "https://pool2.kryptokrona.se", "https://blocksum.org/pool2/stats"],
 
 ];
