6c1e78b9d261d386592972b97e373cdcc014a69a
TechyGuy <65623992+TechyGuy17@users.noreply.github.com>
2022-02-16 19:54:37 +0100

    Update getTransactions.js

--- js/getTransactions.js
+++ js/getTransactions.js
@@ -1,7 +1,7 @@
 async function renderBlocksTransactions(amount) {
   let currentPriceUSD = 0;
-  await getPoolInfo("https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&ids=kryptokrona&order=market_cap_desc&per_page=100&page=1&sparkline=false").then(async(dataB) => {
-    currentPriceUSD = dataB[0].current_price;
+  await getPoolInfo("https://api.coinpaprika.com/v1/tickers/xkr-kryptokrona").then(async(dataB) => {
+    currentPriceUSD = dataB[0].USD.price;
   });
 
   await getLatestBlock().then(async(dataB) => {
